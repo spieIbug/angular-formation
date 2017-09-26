dab8555cc75a3544e17439a3b82963366e2a9141
ymeddah <y.meddah@tmm-groupe.com>
2017-09-26 16:11:18 +0200

    moving to admin-lte dashboard TODO : make a gulp, webpack or any other solution to bundle assets

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+enableProdMode();
 if (environment.production) {
   enableProdMode();
 }
