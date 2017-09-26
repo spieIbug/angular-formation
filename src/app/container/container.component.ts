52d11f4f99a630483c32f2df9a5c1cacbce09e43
ymeddah <y.meddah@tmm-groupe.com>
2017-09-26 16:50:12 +0200

    Integration of `@Input()` for component provided entries

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html'
 })
 export class ContainerComponent implements OnInit {
+
+  @Input()
+  title: string;
+
+  @Input()
+  subTitle: string;
 
   constructor() { }
 
