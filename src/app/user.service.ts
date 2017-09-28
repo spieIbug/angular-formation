2b01f8512252ab7c9654499fc5fd55ffa9e92b34
ymeddah <y.meddah@tmm-groupe.com>
2017-09-28 16:40:03 +0200

    Nothing interessant

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,6 +6,7 @@ export class UserService {
   constructor() { }
 
   isLoggedIn(): boolean {
+    console.warn('i should call an identity server');
     return confirm('a simple yes login \n :)');
   }
 }
